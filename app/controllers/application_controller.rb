3269d4a7da8999a392a225c470536dda440e67ff
Rodrigo Sommacal <rodrigosommacal@gmail.com>
2020-07-08 09:58:00 +0200

    added before action to Application Controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!
+
+  protected
+
   def configure_permitted_parameters
     # For additional fields in app/views/devise/registrations/new.html.erb
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name bio photo])
 
     # For additional in app/views/devise/registrations/edit.html.erb
-  devise_parameter_sanitizer.permit(:account_update, keys: [:name, :bio, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name bio photo])
   end
 end
